2d8a34ff7ff688bd02d38d65ec0410f39297f1c1
Chesnova <myjobistester@gmail.com>
2020-02-10 13:49:04 +0300

    Смена имени

--- sandbox/src/main/java/ru/stqa/pft/sandbox/myfirstprogramm.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/myfirstprogramm.java
@@ -1,8 +1,8 @@
 package ru.stqa.pft.sandbox;
 
 public class myfirstprogramm {
-	
-	public static void main(String[] args) {
-      System.out.println("Hello, world!");
-	}
+
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
 }
