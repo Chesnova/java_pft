9d7dd437d2a77a53126fd968e8b99945b42d50a5
Chesnova <myjobistester@gmail.com>
2020-02-14 01:05:29 +0300

    не нужное

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -2,27 +2,15 @@ package ru.stqa.pft.sandbox;
 
 public class Point {
 
-  int x, y;
+ public double a;
+ public double b;
 
-  public Point (int x, int y){
-    this.x = x;
-    this.y = y;
-  }
+ public Point (double a ,double b) {
+   this.a = a;
+   this.b = b;
+ }
 
-  public static Point scalePoint(Point p, int factor) {
-    Point scaledPoint = new Point(p.x * factor, p.y * factor);
-    return scaledPoint;
-  }
-
-  public static double calculateDistance(Point p1, Point p2){
-    double distance = Math.sqrt(p1.x * p2.x + p1.y * p2.y);
-    return distance;
-  }
-
-  public static void main(String[] args) {
-    Point p = new Point(2,3);
-    Point scaledPoint = scalePoint(p, 10);
-    double distance = calculateDistance(p, scaledPoint);
-    System.out.println(distance);
+  public double distance () {
+    return Math.sqrt(this.a * this.a + this.b * this.b);
   }
 }
