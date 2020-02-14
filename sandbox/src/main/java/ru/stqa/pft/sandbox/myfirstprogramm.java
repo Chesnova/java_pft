abac3805a0cdef11d8944c3e1ef61657bf47e8ff
Chesnova <myjobistester@gmail.com>
2020-02-14 13:12:41 +0300

    Тест для Square

--- sandbox/src/main/java/ru/stqa/pft/sandbox/myfirstprogramm.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/myfirstprogramm.java
@@ -4,6 +4,9 @@ public class MyFirstProgramm {
 
     public static void main(String[] args) {
 
+        Square s = new Square(5);
+        System.out.println("Площадь квадрата со стороной" + s.l + "=" + s.area ());
+
         Point p = new Point(4,9);
         System.out.println("Расстояние между точками" + "=" + p.distance());
     }
