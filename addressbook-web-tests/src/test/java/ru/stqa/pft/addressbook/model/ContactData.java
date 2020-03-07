6bca0698cf4f86ff9edecf4ec173b94bf7dd591b
Chesnova <myjobistester@gmail.com>
2020-03-07 14:39:26 +0300

    4.9 Вычисление максимального индетификатора Контакты

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
@@ -3,7 +3,7 @@ package ru.stqa.pft.addressbook.model;
 import java.util.Objects;
 
 public class ContactData {
-  private final String id;
+  private int id;
   private final String firstName;
   private final String lastName;
   private final String telephone;
@@ -11,19 +11,23 @@ public class ContactData {
   private String group;
 
 
-  public String getId() {
+  public int getId() {
     return id;
   }
 
+  public void setId(int id) {
+    this.id = id;
+  }
+
   public ContactData(String FirstName, String LastName, String Telephone, String EMail, String group) {
-    this.id = null;
+    this.id = 0;
     this.firstName = FirstName;
     this.lastName = LastName;
     this.telephone = Telephone;
     this.EMail = EMail;
     this.group = group;
   }
-  public ContactData(String id, String FirstName, String LastName, String Telephone, String EMail, String group) {
+  public ContactData(int id, String FirstName, String LastName, String Telephone, String EMail, String group) {
     this.id = id;
     this.firstName = FirstName;
     this.lastName = LastName;
@@ -57,7 +61,7 @@ public class ContactData {
     if (this == o) return true;
     if (o == null || getClass() != o.getClass()) return false;
     ContactData that = (ContactData) o;
-    return Objects.equals(id, that.id) &&
+    return id == that.id &&
             Objects.equals(firstName, that.firstName) &&
             Objects.equals(lastName, that.lastName);
   }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -4,6 +4,8 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 
+import java.util.Comparator;
+import java.util.HashSet;
 import java.util.List;
 
 public class ContactCreationTests extends TestBase {
@@ -13,10 +15,13 @@ public class ContactCreationTests extends TestBase {
     app.getNavigationHelper().gotoHomePage();
     app.getNavigationHelper().gotoAddnewPage();
     List<ContactData> before = app.getContactHelper().getContactList();
-    app.getContactHelper().createContact(new ContactData("test", "test", "+79111111111", "test@mail.com", "test1"));
+    ContactData contact = new ContactData("test", "test", "+79111111111", "test@mail.com", "test1");
+    app.getContactHelper().createContact(contact);
     List<ContactData> after = app.getContactHelper().getContactList();
     Assert.assertEquals(after.size(), before.size() + 1);
+
+    contact.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
+    before.add(contact);
+    Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
   }
-
-
 }
