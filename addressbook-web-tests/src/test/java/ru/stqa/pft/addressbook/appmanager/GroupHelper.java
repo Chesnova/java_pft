e5ba5b300c99b57f6dac59d7f372749bc68112d4
Chesnova <myjobistester@gmail.com>
2020-03-10 21:11:16 +0300

    5.5. Повсеместное использование уникальных идентификаторов объектов Группы

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
@@ -5,8 +5,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import ru.stqa.pft.addressbook.model.GroupData;
 
-import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class GroupHelper extends HelperBase {
 
@@ -36,8 +37,8 @@ public class GroupHelper extends HelperBase {
     click(By.name("delete"));
   }
 
-  public void selectGroup(int index) {
-    wd.findElements(By.name("selected[]")).get(index).click();
+  public void selectGroupById(int id) {
+    wd.findElement(By.cssSelector("input[value='" + id + "']")).click();
   }
 
   public void initGroupModification() {
@@ -54,18 +55,19 @@ public class GroupHelper extends HelperBase {
     submitGroupCreation();
     returnToGroupPage();
   }
-  public void modify(int index, GroupData group) {
-    selectGroup(index); // выбор группы
+  public void modify(GroupData group) {
+    selectGroupById(group.getId()); // выбор группы
     initGroupModification(); //начать модификацию
     fillGroupForm(group); // заполнить форму
     submitGroupModification(); // подтвердить
     returnToGroupPage(); // вернуться на главную страницу
   }
 
-  public void delete(int index) {
-    selectGroup(index);
+  public void delete(GroupData group) {
+    selectGroupById(group.getId());
     deleteSelectedGroups();
     returnToGroupPage();
+
   }
 
 
@@ -77,14 +79,15 @@ public class GroupHelper extends HelperBase {
     return wd.findElements(By.name("selected[]")).size();
   }
 
-  public List<GroupData> list() {
-    List<GroupData> groups = new ArrayList<GroupData>();
-    List<WebElement> elements = wd.findElements(By.cssSelector("span.group"));
-    for (WebElement element : elements) {
-      String name = element.getText();
-      int id = Integer.parseInt(element.findElement(By.tagName("input")).getAttribute("value"));
-      groups.add(new GroupData().withId(id).withName(name));
+  public Set<GroupData> all() {
+    Set<GroupData> groups = new HashSet<GroupData>();
+    List<WebElement> elements = wd.findElements(By.cssSelector("span.group")); //все вебэлементы по указанному локатору
+    for (WebElement element : elements) { //для каждого элемента
+      String name = element.getText(); //вытаскиваем имя
+      int id = Integer.parseInt(element.findElement(By.tagName("input")).getAttribute("value")); //вытаскиваем идетификатор группы
+      groups.add(new GroupData().withId(id).withName(name)); //создаем новый объект и помещаем его в множество
     }
     return groups;
   }
+
 }
