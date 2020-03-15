6384140fca9a1ce49d047164107d62fe3c227726
Chesnova <myjobistester@gmail.com>
2020-03-16 00:45:55 +0300

    Задание №10. Реализация проверок в fluent-стиле Группы

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -1,11 +1,13 @@
 package ru.stqa.pft.addressbook.tests;
 
-import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
+import ru.stqa.pft.addressbook.model.Groups;
 
-import java.util.Set;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.testng.Assert.assertEquals;
 
 public class GroupModificationTests extends TestBase {
 
@@ -19,18 +21,15 @@ public class GroupModificationTests extends TestBase {
 
   @Test
   public void testGroupModification () {
-    Set<GroupData> before = app.group().all(); //количество групп до модификации
+    Groups before = app.group().all(); //количество групп до модификации
     GroupData modifiedGroup = before.iterator().next();
     GroupData group = new GroupData()
             .withId(modifiedGroup.getId()).withName("test1").withHeader("test2").withFooter("test3");
     app.group().modify(group);
-    Set<GroupData> after = app.group().all();
-    Assert.assertEquals(after.size(), before.size()); //количество групп после модификации
+    Groups after = app.group().all();
+    assertEquals(after.size(), before.size()); //количество групп после модификации
 
-    before.remove(modifiedGroup);
-    before.add(group);
-    Assert.assertEquals(before, after);
+    assertThat(after, equalTo(before.without(modifiedGroup).withAdded(group)));
   }
-
 
 }
