f7d095537d33c31995392252426ef5fe0c7f20d5
Chesnova <myjobistester@gmail.com>
2020-03-20 18:30:29 +0300

    Лекция 6.4. Параметризация тестовых методов Контакты

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -1,23 +1,32 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactCreationTests extends TestBase {
 
-  @Test ( enabled=false )
-  public void testContactCreation() throws Exception {
+  @DataProvider
+  public Iterator<Object[]> validContacts() {
+    List<Object[]> list = new ArrayList<Object[]>();
+    list.add(new Object[] {new ContactData().withLastName("test").withFirstName("test").withAddress("address").withHomePhone("HomePhone").withMobilePhone("MobilePhone").withWorkPhone("WorkPhone").withEMail2("EMail2").withEMail3("EMail3")});
+//    list.add(new Object[] {new ContactData().withLastName("test2").withFirstName("test2").withAddress("address2").withHomePhone("HomePhone2").withMobilePhone("MobilePhone2").withWorkPhone("WorkPhone2").withEMail2("EMail22").withEMail3("EMail32")});
+//    list.add(new Object[] {new ContactData().withLastName("test3").withFirstName("test3").withAddress("address3").withHomePhone("HomePhone3").withMobilePhone("MobilePhone3").withWorkPhone("WorkPhone3").withEMail2("EMail23").withEMail3("EMail33")});
+    return list.iterator();
+  }
+
+  @Test (dataProvider = "validContacts")
+  public void testContactCreation(ContactData contact)  {
     Contacts before = app.contact().all(); //считаем количество контактов до добавления?
     app.goTo().addnewPage();
-    ContactData contact = new ContactData()
-            .withLastName("test").withFirstName("test")
-            .withMobilePhone("+79111111111")
-            .withAddress("198000, Saint Petersburg,\n" + "Nevsky prospekt, 25/3-43\n" + "domofon 43")
-            .withEMail("test@mail.com").withGroup("test1");
     app.contact().create(contact);
     app.goTo().homePage();
     assertThat(app.contact().count(), equalTo(before.size() + 1));
@@ -25,7 +34,8 @@ public class ContactCreationTests extends TestBase {
     assertThat(after, equalTo(
             before.withAdded(contact.withId(after.stream().mapToInt((c) -> c.getId()).max().getAsInt()))));
   }
-  @Test
+
+  @Test ( enabled=false )
   public void testContactCreation2() throws Exception {
     app.goTo().addnewPage();
     File photo = new File("src/test/resources/stru.png");
