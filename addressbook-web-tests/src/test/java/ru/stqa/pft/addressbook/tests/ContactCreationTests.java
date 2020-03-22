b876bf749304e7f280bf9bad63e5adba2b0243bc
Chesnova <myjobistester@gmail.com>
2020-03-22 15:24:51 +0300

    Лекция 6.5. Загрузка тестовых данных из файла Контакты

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -4,7 +4,8 @@ import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
-import java.io.File;
+
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -15,17 +16,22 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class ContactCreationTests extends TestBase {
 
   @DataProvider
-  public Iterator<Object[]> validContacts() {
+  public Iterator<Object[]> validContacts() throws IOException {
     List<Object[]> list = new ArrayList<Object[]>();
-    list.add(new Object[] {new ContactData().withLastName("test").withFirstName("test").withAddress("address").withHomePhone("HomePhone").withMobilePhone("MobilePhone").withWorkPhone("WorkPhone").withEMail2("EMail2").withEMail3("EMail3")});
-//    list.add(new Object[] {new ContactData().withLastName("test2").withFirstName("test2").withAddress("address2").withHomePhone("HomePhone2").withMobilePhone("MobilePhone2").withWorkPhone("WorkPhone2").withEMail2("EMail22").withEMail3("EMail32")});
-//    list.add(new Object[] {new ContactData().withLastName("test3").withFirstName("test3").withAddress("address3").withHomePhone("HomePhone3").withMobilePhone("MobilePhone3").withWorkPhone("WorkPhone3").withEMail2("EMail23").withEMail3("EMail33")});
+    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.csv")));
+    String line = reader.readLine();
+    while (line != null) {
+      String[] split = line.split(";");
+      list.add(new Object[] {new ContactData().withLastName(split[0]).withFirstName(split[1]).withAddress(split[2]).withHomePhone(split[3]).withMobilePhone(split[4]).withWorkPhone(split[5]).withEMail(split[6]).withEMail2(split[7]).withEMail3(split[8])});
+      line = reader.readLine();
+    }
     return list.iterator();
   }
 
   @Test (dataProvider = "validContacts")
   public void testContactCreation(ContactData contact)  {
-    Contacts before = app.contact().all(); //считаем количество контактов до добавления?
+    app.goTo().homePage();
+    Contacts before = app.contact().all();
     app.goTo().addnewPage();
     app.contact().create(contact);
     app.goTo().homePage();
@@ -50,7 +56,7 @@ public class ContactCreationTests extends TestBase {
 
   @Test ( enabled=false )
   public void testBadContactCreation() throws Exception {
-    Contacts before = app.contact().all(); //считаем количество контактов до добавления?
+    Contacts before = app.contact().all();
     app.goTo().addnewPage();
     ContactData contact = new ContactData()
             .withLastName("test'").withFirstName("test").withTelephone("+79111111111").withEMail("test@mail.com").withGroup("test1");
