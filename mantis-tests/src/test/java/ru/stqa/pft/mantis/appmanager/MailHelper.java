1d80d31e07c408eb51ce60a40a65fac6959f5c08
Chesnova <myjobistester@gmail.com>
2020-04-10 19:09:36 +0300

    Лекция 8.6. Получение почты на встроенный в тесты почтовый сервер

---
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/MailHelper.java
@@ -0,0 +1,61 @@
+package ru.stqa.pft.mantis.appmanager;
+
+import org.subethamail.wiser.Wiser;
+import org.subethamail.wiser.WiserMessage;
+import ru.stqa.pft.mantis.model.MailMessage;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class MailHelper {
+  private ApplicationManager app;
+  private final Wiser wiser;
+
+  public MailHelper(ApplicationManager app) {
+    this.app = app;
+    wiser = new Wiser(); //при инициализации создается объект типа wiser
+  }
+
+
+  public List<MailMessage> waitForMail(int count, long timeout) throws MessagingException, IOException {
+    long start = System.currentTimeMillis(); // сначала запоминаем текущее время
+    //затем устраиваем цикл while, в котором проверяется что новое текущее время не превышает момент старта + таймаут
+    while (System.currentTimeMillis() < start + timeout) {
+      //если почты пришло достаточно много, то можно прекращать
+      if (wiser.getMessages().size() >= count) {
+        return wiser.getMessages().stream().map((m) -> toModelMail(m)).collect(Collectors.toList());
+      }
+      //если почты мало, то проскакиваем проверку и перескакиваем сюда
+      try {
+        Thread.sleep(1000); // подождать и сделаем новый заход в цикл
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+    }
+    throw new Error("No mail :(");
+  }
+
+  public static MailMessage toModelMail(WiserMessage m) {
+    try {
+      MimeMessage mm = m.getMimeMessage();
+      return new MailMessage(mm.getAllRecipients()[0].toString(), (String) mm.getContent());
+    } catch (MessagingException e) {
+      e.printStackTrace();
+      return null;
+    } catch (IOException e) {
+      e.printStackTrace();
+      return null;
+    }
+  }
+
+  public void start() {
+    wiser.start();
+  }
+
+  public void stop() {
+    wiser.stop();
+  }
+}
