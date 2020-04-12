09427edd994cbdd7a53a29c6a7bb8028ce334ad9
Chesnova <myjobistester@gmail.com>
2020-04-12 22:45:35 +0300

    Лекция 8.5. Передача файлов на удалённую машину по протоколу FTP. Удаление Капчи

---
+++ mantis-tests/build/resources/test/config_inc.php
@@ -0,0 +1,9 @@
+<?php
+$g_hostname               = 'localhost';
+$g_db_type                = 'mysqli';
+$g_database_name          = 'bugtracker';
+$g_db_username            = 'root';
+$g_db_password            = '';
+$g_crypto_master_salt     = 'Lp/NZfl3E/QRf3LcQgGdDM4keNmnrehjSEPnu+ES0n4=';
+$g_signup_use_captcha = OFF;
+
