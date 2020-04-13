7572457ce25cf7fd7bcef5ca1cb26d46c181eb29
Chesnova <myjobistester@gmail.com>
2020-04-13 12:40:18 +0300

    RegistrationHelper исправлены локаторы

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/RegistrationHelper.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/RegistrationHelper.java
@@ -13,14 +13,14 @@ public class RegistrationHelper extends HelperBase {
     wd.get(app.getProperty("web.baseUrl") + "/signup_page.php");
     type(By.name("username"), username);
     type(By.name("email"), email);
-    click(By.cssSelector("span.bigger-110")); // исправить название локатора  Signup //input[value='Зарегистрироваться']
+    click(By.cssSelector("input[type='submit']"));
   }
 
   public void finish(String confirmationLink, String password) {
     wd.get(confirmationLink);
     type(By.name("password"), password);
     type(By.name("password_confirm"), password);
-    click(By.cssSelector("input[value='Update User']")); // проверить в браузере название кнопки
+    click(By.cssSelector("span.bigger-110"));
   }
 
 }
