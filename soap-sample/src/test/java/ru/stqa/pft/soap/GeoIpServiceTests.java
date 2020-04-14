608e17b446c55bbc73edc47e0f7448beca96619d
Chesnova <myjobistester@gmail.com>
2020-04-14 16:50:10 +0300

    testMyIp правильный

--- soap-sample/src/test/java/ru/stqa/pft/soap/GeoIpServiceTests.java
+++ soap-sample/src/test/java/ru/stqa/pft/soap/GeoIpServiceTests.java
@@ -1,9 +1,10 @@
 package ru.stqa.pft.soap;
 
 import com.lavasoft.GeoIPService;
+import com.lavasoft.GeoIPServiceSoap;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import static org.testng.Assert.assertEquals;
 
 public class GeoIpServiceTests {
 
@@ -11,6 +12,10 @@ public class GeoIpServiceTests {
     public void testMyIp() {
     //объект типа геосервис, выбираем тип реализации, и вызываем функцию гет, результат сохраняем в переменную iplоcation
     String ipLocation = new GeoIPService().getGeoIPServiceSoap12().getIpLocation("95.55.109.137");
-    assertEquals(ipLocation,"RUS");
+    //так и должно быть, потому что именно такое составное значение возвращает этот сервис. не объект (как тот, ранее существовавший сервис), а строку с тегами
+     //поэтому можно её так целиком и сравнивать
+    // если подставить просто RUS, то выдаст expected [RUS] but found [<GeoIP><Country>RU</Country><State>66</State></GeoIP>]
+    //java.lang.AssertionError: expected [RUS] but found [<GeoIP><Country>RU</Country><State>66</State></GeoIP>]
+    Assert.assertEquals(ipLocation,"<GeoIP><Country>RU</Country><State>66</State></GeoIP>");
   }
 }
