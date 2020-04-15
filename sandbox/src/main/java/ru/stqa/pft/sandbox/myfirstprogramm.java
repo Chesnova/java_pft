cae868617396ae548fa3ce741bd8c54ef6a846bc
Chesnova <myjobistester@gmail.com>
2020-04-15 21:13:58 +0300

    проверка теста

--- sandbox/src/main/java/ru/stqa/pft/sandbox/myfirstprogramm.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/myfirstprogramm.java
@@ -1,13 +1,16 @@
 package ru.stqa.pft.sandbox;
 
-public class MyFirstProgramm {
+import org.testng.annotations.Test;
 
-    public static void main(String[] args) {
+public class myfirstprogramm {
+
+  @Test
+    public void main(String[] args) {
 
         Square s = new Square(5);
         System.out.println("Площадь квадрата со стороной" + s.l + "=" + s.area ());
 
-      Point p1 = new Point(3,4);
+      Point p1 = new Point(6,6);
       Point p2 = new Point(7,8);
       System.out.println(p1.distance(p2));
     }
--- sandbox/src/test/java/ru/stqa/pft/sandbox/SquareTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/SquareTests.java
@@ -7,7 +7,7 @@ public class SquareTests {
 
   @Test
   public void testArea (){
-    Square s = new Square (7);
-    Assert.assertEquals ( s.area(), 49.0);
+    Square s = new Square (4);
+    Assert.assertEquals ( s.area(), 12.0);
   }
 }
